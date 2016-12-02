846d41f90af0276f8d3fa4f65de34a358dc7e707
vladimir.saldivar <vsaldivarm@gmail.com>
2016-12-02 13:54:39 -0600

    First commit

---
+++ src/main/java/com/example/spartdark/ReadingListDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.spartdark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingListDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListDemoApplication.class, args);
+	}
+}
